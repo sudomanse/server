1d7ecda3624494bd4c745cb8e27eca132345f756
Werner Neethling <wernertn@gmail.com>
2019-11-18 20:35:27 -0500

    Ready for testing 0.1.0
    Added all routers.
    Setup middleware.
    Setup helpers.

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import { CONFIG } from "./config";
 // import Logger
 import { Logger } from "./Logger";
 
+// import fs
+import { readFileSync } from "fs";
+
 // import typeorm
 import { createConnection } from "typeorm";
 
@@ -11,9 +14,21 @@ import { createConnection } from "typeorm";
 import * as Koa from "koa";
 import * as bodyparser from "koa-bodyparser";
 import * as json from "koa-json";
+import * as cors from "@koa/cors";
 
 // import routers
+import { authRouter } from "./router/Auth";
+import { inputRouter } from "./router/Input";
+import { outputRouter } from "./router/Output";
+// import { readingRouter } from "./router/Reading";
+import { stationRouter } from "./router/Station";
+import { userRouter } from "./router/User";
 
+// import helpers
+import { MqttClient } from "./helper/MqttClient";
+import { JWT } from "./helper/JWT";
+
+// start server
 const startServer = async () => {
     try {
         // create connection to database
@@ -22,25 +37,48 @@ const startServer = async () => {
         // create app
         const app = new Koa();
 
+        // create MQTT helper
+        const mqtt = MqttClient;
+        await mqtt.start();
+
+        // create JWT helper
+        const privateKey = await readFileSync(CONFIG.JWT_PRIVATE_KEY_LOCATION);
+        const publicKey = await readFileSync(CONFIG.JWT_PUBLIC_KEY_LOCATION);
+        JWT.init(privateKey,  publicKey, "HS256");
+
+        app.use(cors());
+
         // apply middleware
         app.use(bodyparser({
             enableTypes: ["json"],
-            jsonLimit: CONFIG.JSON_LIMIT,
+            jsonLimit: CONFIG.BODYPARSER_JSON_LIMIT,
             strict: true
         }));
         app.use(json());
 
         // apply routers
+        app.use(inputRouter.routes());
+        app.use(inputRouter.allowedMethods());
+        app.use(outputRouter.routes());
+        app.use(outputRouter.allowedMethods());
+        // app.use(readingRouter.routes());
+        // app.use(readingRouter.allowedMethods());
+        app.use(stationRouter.routes());
+        app.use(stationRouter.allowedMethods());
+        app.use(userRouter.routes());
+        app.use(userRouter.allowedMethods());
+        app.use(authRouter.routes());
+        app.use(authRouter.allowedMethods());
 
         // listen on port
-        app.listen(CONFIG.PORT, () => {
-            Logger.INFO(`Listening on :${CONFIG.PORT}`);
+        app.listen(CONFIG.SERVER_PORT, () => {
+            Logger.INFO(`Listening on :${CONFIG.SERVER_PORT}`);
         });
+
+        return app;
     }
     catch (error) {
-        Logger.ERRO(`Error starting server, ${error}`);
+        Logger.CRIT(`Error starting server, ${error}`);
     }
 };
-
-// start server
-startServer();
+const app = startServer();
